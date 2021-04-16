3107d4437d63c5eb0d7b5b746e74a06e179106d3
Benjamin Krzis <benjamin@elisana.digital>
2021-04-16 14:14:09 +0200

    Mount appheader setdata login true and find button to be false

--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -1,11 +1,15 @@
+import { mount } from "@vue/test-utils";
 import AppHeader from "@/components/AppHeader";
 
 describe("AppHeader", () => {
   test("if a user is not logged in, do not show the logout button", () => {
-    expect(true).toBe(true);
+    const wrapper = mount(AppHeader); // mounting the component
+    expect(wrapper.find("button").isVisible()).toBe(false);
   });
 
-  test("if a user is logged in, show the logout button", () => {
-    expect(true).toBe(true);
+  test("if a user is logged in, show the logout button", async () => {
+    const wrapper = mount(AppHeader); // mounting this compony aswell
+    await wrapper.setData({ loggedIn: true }); // setting our data value
+    expect(wrapper.find("button").isVisible()).toBe(true);
   });
 });
